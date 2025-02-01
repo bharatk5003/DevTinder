05e50228f3b647311926d74c9c37f0abbe0a6efa
Bharat Kumar <bharatk503@gmail.com>
2025-02-02 02:11:13 +0530

    schema created and database connection estableshed

--- src/app.js
+++ src/app.js
@@ -1,13 +1,11 @@
-const express = require('express');
-const connectDB=require('./config/database')
-const app= express();
+const express = require("express");
+const connectDB = require("./config/database");
+const app = express();
 
+connectDB().then(() => {
+  console.log("database connection established");
 
-connectDB().then(()=>{
-    console.log("database connection established");
-
-    app.listen(3000,()=>{
-        console.log(`App is running on port 3000`);
-     })
-})
-
+  app.listen(3000, () => {
+    console.log(`App is running on port 3000`);
+  });
+});
---
+++ src/config/database.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+
+
+const connectDB = async ()=>{
+await mongoose.connect("mongodb://localhost:27017/devTinder").catch((err)=>{
+    console.log(`error while connecting to database ${err}`)
+})
+
+mongoose.connection.on('connected', () => console.log('connected'));
+mongoose.connection.on('open', () => console.log('open'));
+mongoose.connection.on('disconnected', () => console.log('disconnected'));
+mongoose.connection.on('reconnected', () => console.log('reconnected'));
+mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
+mongoose.connection.on('close', () => console.log('close'));
+
+};
+
+
+module.exports= connectDB;
+
