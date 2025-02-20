fbd2e1178a9876bc62a22191d89fa826098975a0
Bharat Kumar <bharatkumar@fynd-external.com>
2025-02-20 16:25:59 +0530

    get post api created

--- src/app.js
+++ src/app.js
@@ -18,6 +18,39 @@ app.use("/signup", async (req, res) => {
 
 })
 
+app.get("/getUsers", async (req, response) => {
+  try {
+    const users = await userModel.find({});
+    console.log(res);
+    response.status(200).send(users);
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+app.get("/getUserById", async (req, res) => {
+  const { emailId } = req.query;
+  try {
+    const user = await userModel.find({ emailId })
+    res.status(200).send(user);
+  } catch (error) {
+    console.log(error);
+    res.send(error);
+  }
+})
+
+app.delete("/delete", async (req, res) => {
+  const { emailId } = req.query;
+  try {
+    console.log(emailId);
+    const user = userModel.deleteOne({ emailId });
+    console.log(user);
+    res.status(200).send(user)
+  } catch (error) {
+    res.send(error)
+  }
+})
+
 connectDB().then(() => {
   console.log("database connection established");
 
--- src/config/database.js
+++ src/config/database.js
@@ -2,20 +2,20 @@ const mongoose = require('mongoose');
 
 
 
-const connectDB = async ()=>{
-await mongoose.connect("mongodb://localhost:27017/devTinder").catch((err)=>{
-    console.log(`error while connecting to database ${err}`)
-})
+const connectDB = async () => {
+    await mongoose.connect("mongodb+srv://bharatk5003:Megatron@bharat.arwrijo.mongodb.net/?retryWrites=true&w=majority&appName=Bharat/devTinder").catch((err) => {
+        console.log(`error while connecting to database ${err}`)
+    })
 
-mongoose.connection.on('connected', () => console.log('connected'));
-mongoose.connection.on('open', () => console.log('open'));
-mongoose.connection.on('disconnected', () => console.log('disconnected'));
-mongoose.connection.on('reconnected', () => console.log('reconnected'));
-mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
-mongoose.connection.on('close', () => console.log('close'));
+    mongoose.connection.on('connected', () => console.log('connected'));
+    mongoose.connection.on('open', () => console.log('open'));
+    mongoose.connection.on('disconnected', () => console.log('disconnected'));
+    mongoose.connection.on('reconnected', () => console.log('reconnected'));
+    mongoose.connection.on('disconnecting', () => console.log('disconnecting'));
+    mongoose.connection.on('close', () => console.log('close'));
 
 };
 
 
-module.exports= connectDB;
+module.exports = connectDB;
 
